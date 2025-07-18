930cb595818faeca85e5e60b8438de2703e7ea90
psjw <dlrudtn108@gmail.com>
2025-07-18 19:46:25 +0900

    실무활용 - 순수 JPA와 Querydsl - 순수 JPA리포지토리와 Querydsl

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,10 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +13,10 @@ public class QuerydslApplication {
         SpringApplication.run(QuerydslApplication.class, args);
     }
 
+
+    //동시성 문제 없음 -> EntityManager는 스프링에서 가짜 Proxy 객체를 넣어줌 -> 트랜잭션단위로 다른데 바인딩되도록 바인딩해줌 -> 책 13.1 참고
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
